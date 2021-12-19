1de13cb70281d4563e6f0c4676f651ed12102c9f
Maria Vostrikova <vostrikova.maria2017@yandex.ru>
2021-12-20 01:44:56 +0300

    tests are successfully passed, blocked user added

--- src/test/java/ru/netology/Test/Data.java
+++ src/test/java/ru/netology/Test/Data.java
@@ -1,7 +1,6 @@
 package ru.netology.Test;
 
 import com.github.javafaker.Faker;
-import com.google.gson.Gson;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.specification.RequestSpecification;
@@ -9,14 +8,12 @@ import lombok.Value;
 import static io.restassured.RestAssured.given;
 
 public class Data {
+    private static final Faker faker = new Faker();
+
     private Data() {
     }
-public static class UserUser{
-    private static final Faker faker = new Faker();
-    private static final String username = faker.name().username();
-    private static final String password = faker.internet().password();
 
-    private static RequestSpecification requestSpec = new RequestSpecBuilder()
+    private static final RequestSpecification requestSpec = new RequestSpecBuilder()
             .setBaseUri("http://localhost")
             .setPort(9999)
             .setAccept(io.restassured.http.ContentType.JSON)
@@ -24,47 +21,60 @@ public static class UserUser{
             .log(LogDetail.ALL)
             .build();
 
-    public static void validUser() {
+    static void sendRequest(Registration.User user) {
         // сам запрос
         given() // "дано"
                 .spec(requestSpec) // указываем, какую спецификацию используем
-                .body(new Gson().toJson(new User(username, password, "active"))) // передаём в теле объект, который будет преобразован в JSON
+                .body(user) // передаём в теле объект, который будет преобразован в JSON
                 .when() // "когда"
                 .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
                 .then() // "тогда ожидаем"
                 .statusCode(200); // код 200 OK
     }
 
-    public static void invalidUser() {
-        // сам запрос
-        given() // "дано"
-                .spec(requestSpec) // указываем, какую спецификацию используем
-                .body(new Gson().toJson(new User(username, password, "blocked"))) // передаём в теле объект, который будет преобразован в JSON
-                .when() // "когда"
-                .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
-                .then() // "тогда ожидаем"
-                .statusCode(200); // код 200 OK
-    }
-        public static String getUsername() {
-            return username;
+    public static class Registration {
+        private Registration() {
         }
 
-        public static String getPassword() {
-            return password;
-        }
-
-        public static String getAnotherUsername() {
+        public static String generateLogin() {
             return faker.name().username();
         }
 
-        public static String getAnotherPassword() {
+        public static String generatePassword() {
             return faker.internet().password();
         }
-    }
+
+        public static User generateValidUser() {
+            User user = new User(generateLogin(), generatePassword(), "active");
+            sendRequest(user);
+            return user;
+        }
+
+        public static User generateBlockedUser() {
+            User user = new User(generateLogin(), generatePassword(), "blocked");
+            sendRequest(user);
+            return user;
+        }
+
+        public static User generateInvalidPassword(String status) {
+            String login = generateLogin();
+            sendRequest(new User(login, generatePassword(), status));
+            return new User(login, generatePassword(), status);
+        }
+
+        public static User generateInvalidLogin(String status) {
+            String password = generatePassword();
+            sendRequest(new User(generateLogin(), password, status));
+            return new User(generateLogin(), password, status);
+        }
+
         @Value
-        private static class User {
+        public static class User {
             String login;
             String password;
             String status;
         }
     }
+}
+
+
--- src/test/java/ru/netology/Test/UITest.java
+++ src/test/java/ru/netology/Test/UITest.java
@@ -3,50 +3,52 @@ package ru.netology.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
-import static ru.netology.Test.Data.UserUser.*;
+import static ru.netology.Test.Data.Registration.*;
+
 
 public class UITest {
 
     @BeforeEach
     void setup() {
-        validUser();
         open("http://localhost:9999");
     }
 
     @Test
     void shouldHaveValidData() {
-        $("[data-test-id='login'] .input__control").setValue(getUsername());
-        $("[data-test-id='password'] .input__control").setValue(getPassword());
+        User validUser = Data.Registration.generateValidUser();
+        $("[data-test-id=login] input").setValue(validUser.getLogin());
+        $("[data-test-id=password] input").setValue(validUser.getPassword());
         $("button[data-test-id=action-login]").click();
         $(".App_appContainer__3jRx1").shouldBe(visible).shouldHave(text("Личный кабинет"));
     }
 
     @Test
     void shouldHaveBlockedUser() {
-        invalidUser();
-        $("[data-test-id='login'] .input__control").setValue(getAnotherUsername());
-        $("[data-test-id='password'] .input__control").setValue(getAnotherPassword());
+        User blockedUser = generateBlockedUser();
+        $("[data-test-id=login] input").setValue(blockedUser.getLogin());
+        $("[data-test-id=password] input").setValue(blockedUser.getPassword());
         $("button[data-test-id=action-login]").click();
-        $("[data-test-id=error-notification] .notification__content").shouldHave(text("Ошибка! Неверно указан логин или пароль"));
-    }
-
-    @Test
-    void shouldFakeUsername() {
-        invalidUser();
-        $("[data-test-id='login'] .input__control").setValue(getAnotherUsername());
-        $("[data-test-id='password'] .input__control").setValue(getPassword());
-        $("button[data-test-id=action-login]").click();
-        $("[data-test-id=error-notification] .notification__content").shouldHave(text("Ошибка! Неверно указан логин или пароль")).shouldBe(visible);
+        $(withText("Пользователь заблокирован")).shouldBe(visible);
     }
 
     @Test
     void shouldFakePassword() {
-        invalidUser();
-        $("[data-test-id='login'] .input__control").setValue(getUsername());
-        $("[data-test-id='password'] .input__control").setValue(getAnotherPassword());
+        User fakePassword = generateInvalidPassword("active");
+        $("[data-test-id=login] input").setValue(fakePassword.getLogin());
+        $("[data-test-id=password] input").setValue(fakePassword.getPassword());
         $("button[data-test-id=action-login]").click();
-        $("[data-test-id=error-notification] .notification__content").shouldHave(text("Ошибка! Неверно указан логин или пароль")).shouldBe(visible);
+        $("[data-test-id=error-notification] .notification__content").shouldHave(text("Неверно указан логин или пароль")).shouldBe(visible);
+    }
+
+    @Test
+    void shouldFakeUsername() {
+        User fakeLogin = generateInvalidLogin("active");
+        $("[data-test-id=login] input").setValue(fakeLogin.getLogin());
+        $("[data-test-id=password] input").setValue(fakeLogin.getPassword());
+        $("button[data-test-id=action-login]").click();
+        $("[data-test-id=error-notification] .notification__content").shouldHave(text("Неверно указан логин или пароль")).shouldBe(visible);
     }
 }
 
